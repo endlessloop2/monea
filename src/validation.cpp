4a1616f24daf71edc0fe2ff68ecd990feb61106a
endlessloop2 <alaughre@mail.ccsf.edu>
2018-08-25 14:23:55 -0300

    Multialgo validation

--- src/validation.cpp
+++ src/validation.cpp
@@ -1151,8 +1151,21 @@ bool GetTransaction(const uint256 &hash, CTransaction &txOut, const Consensus::P
 bool CheckProofOfWork(const CBlockHeader& block, const Consensus::Params& params)
 {
 
+    /* without multi-algo
     if (!CheckProofOfWork(block.GetPoWHash(), block.nBits, params))
         return error("%s (val.cpp): proof of work failed", __func__);
+    */
+    // Based off an auxpow coin, that's the reason for this pointless function
+    int algo = block.GetAlgo();
+    if (!CheckProofOfWork(block.GetPoWHash(algo, params), algo, block.nBits, params))
+        return error("%s (val.cpp) : non-AUX proof of work failed, hash=%s, algo=%d, nVersion=%d, PoWHash=%s",
+        __func__,
+        block.GetHash().ToString(),
+        algo,
+        block.nVersion,
+        block.GetPoWHash(algo, params).ToString());
+
+
     return true;
 }
 
@@ -3244,22 +3257,13 @@ static bool CheckIndexAgainstCheckpoint(const CBlockIndex* pindexPrev, CValidati
 bool ContextualCheckBlockHeader(const CBlockHeader& block, CValidationState& state, CBlockIndex * const pindexPrev)
 {
     const Consensus::Params& consensusParams = Params().GetConsensus();
-    int nHeight = pindexPrev->nHeight + 1;
+    int nHeight = pindexPrev->nHeight + 1
+    
     // Check proof of work
-
-    if(Params().NetworkIDString() == CBaseChainParams::MAIN && nHeight <= 68589){
-        // architecture issues with DGW v1 and v2)
-        unsigned int nBitsNext = GetNextWorkRequired(pindexPrev, &block, consensusParams);
-        double n1 = ConvertBitsToDouble(block.nBits);
-        double n2 = ConvertBitsToDouble(nBitsNext);
-        if (abs(n1-n2) > n1*0.5)
-            return state.DoS(100, error("%s : incorrect proof of work (DGW pre-fork) - %f %f %f at %d", __func__, abs(n1-n2), n1, n2, nHeight),
-                            REJECT_INVALID, "bad-diffbits");
-    } else {
-        if (block.nBits != GetNextWorkRequired(pindexPrev, &block, consensusParams))
-            return state.DoS(100, error("%s : incorrect proof of work at %d", __func__, nHeight),
-                            REJECT_INVALID, "bad-diffbits");
-    }
+    int algo = block.GetAlgo();
+    if (block.nBits != GetNextWorkRequired(pindexPrev, &block, algo, consensusParams))
+        return state.DoS(100, error("%s : incorrect proof of work at %d", __func__, nHeight),
+            REJECT_INVALID, "bad-diffbits");
 
     // Check timestamp against prev
     if (block.GetBlockTime() <= pindexPrev->GetMedianTimePast())
